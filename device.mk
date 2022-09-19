d7330e3e9ac1c0f67571c4cb593614999616b5aa
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2022-09-19 21:49:52 +0000

    onclite: Build mock hidl power stats * Fix for hidl power stats errors. - Now with this we fixing both errors: BatteryStatsService: Unable to load Power Hal or power.stats HAL hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest. hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- device.mk
+++ device.mk
@@ -344,6 +344,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service.onclite-libperfmgr
 
 PRODUCT_COPY_FILES += \
